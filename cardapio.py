e8ead4ac0c9edb570f2fe15d9feded5daa2a3553
manuelaaguiargil06 <manuelaaguiargil06@gmail.com>
2025-11-28 22:59:11 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id':1,
+            'titulo':'Bolo de Chocolate',
+            'descricao':'delicioso bolo de chocolaate',
+            'foto':''
+        },
+        {
+            'id':2,
+            'titulo':'Bolo de Morango',
+            'descricao':'delicioso bolo de morango',
+            'foto':''
+        }
+    ]
+    return cardapio
