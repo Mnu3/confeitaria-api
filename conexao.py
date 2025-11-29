a2623ab7228aade7011ee8e8bbe8cb108da95235
manuelaaguiargil06 <manuelaaguiargil06@gmail.com>
2025-11-29 00:32:29 +0000

    conexao

--- conexao.py
+++ conexao.py
@@ -1,10 +1,9 @@
 import psycopg2
-from conexao import get_conexao
 
 def get_conexao():
     conn = psycopg2.connect(
         dbname='postgres',
-        user='postgres.mliaizbqrzstuzleuqvf',
+        user='postgres.royfkjapbmqrdoprlydy',
         password='CapivariIfsp',
         host='aws-1-us-east-2.pooler.supabase.com',
         port=6543
